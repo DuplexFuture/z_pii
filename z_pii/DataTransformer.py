c31b999ec4b52f1c50f8b3bad3c7adc1d90cc80d
Jack Housego <jack.housego@btinternet.com>
2022-07-14 17:54:05 +0100

    remove bug - test should pass on git

--- z_pii/DataTransformer.py
+++ z_pii/DataTransformer.py
@@ -19,19 +19,10 @@ class DataTransformer():
         """
         with jsonlines.open(self.file_path) as f:
             self.data = []
-            # for data_record in f.iter():
-            #     data_record = self.remove_name(data_record)
-            #     data_record = self.retreive_domain(data_record)
-            #     self.data.append(data_record)
-            count = 0
             for data_record in f.iter():
-                if count == 4:
-                    self.data.append(data_record)
-                else:
-                    data_record = self.remove_name(data_record)
-                    data_record = self.retreive_domain(data_record)
-                    self.data.append(data_record)
-                count += 1
+                data_record = self.remove_name(data_record)
+                data_record = self.retreive_domain(data_record)
+                self.data.append(data_record)
 
     def remove_name(self, data_record):
         """
